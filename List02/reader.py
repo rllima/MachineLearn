2e606359ed7d8e352a558516113e55ee816ad6db
rllima <rlo@cin.ufpe.br>
2018-04-11 01:08:35 -0300

    LVQ1 almost done

--- List02/reader.py
+++ List02/reader.py
@@ -1,10 +1,19 @@
 import pandas as pd
 from scipy.io import arff
+from sklearn import preprocessing
 
 
 def readBase():
     data = arff.loadarff('F:\AM\Listas\MachineLearn\List02\DataBases\KC1.arff')
     df = pd.DataFrame(data[0])
+    numberOfColumns = len(df.columns)
+    justClass = df[['defects']].values     #only the classes
+    justData = df.drop(['defects'], axis=1)    #Remove the last column for normalization
+    justaDataValues = justData.values #returns a numpy array
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(justaDataValues)
+    df = pd.DataFrame(x_scaled)
+    df.insert(loc=numberOfColumns-1, column='defects', value=justClass)
     return df
     
 
--- List02/test.py
+++ List02/test.py
@@ -1,24 +1,27 @@
 
 import numpy as np
 import reader as rd
+import numbers
 import kfold as kfold
 import prototypes
 import lvq1
-
+from sklearn.preprocessing import MinMaxScaler
 
 dataset = rd.readBase()
 dataset = dataset.values
 dataset = np.array(dataset)
-
 Y = kfold.getDefaultResults(dataset)
 numberOfClasses = len(set(Y))
 classesValues = list(set(Y))
+# scaler = MinMaxScaler()
+# data = []
+# print(scaler.data_max_)
+
 k = kfold.generateFolds(dataset,2)
 for train_index, test_index in k.split(dataset,Y):
         X_train, X_test = dataset[train_index], dataset[test_index]
         print("TRAIN:", X_train.shape[0], "TEST:", X_test.shape[0])
-        x = prototypes.generatePrototypes(X_train,numberOfClasses,classesValues,10)
-        for i in range(len(X_train)):
-                d = lvq1.calculateDistance(X_train[i], x)
-       
+        prot = prototypes.generatePrototypes(X_train,numberOfClasses,classesValues,10)
+        LVQ_Prototypes = lvq1.train(X_train, 0.1, prot,5)
+        print(LVQ_Prototypes)
 
